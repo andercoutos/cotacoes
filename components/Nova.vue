accc20a4ba32f9042fd4399a3ed4a4a8970c5f1c
Anderson Coutos <84669018+andercoutos@users.noreply.github.com>
2022-04-29 10:56:50 -0300

    api com jquery

--- components/Nova.vue
+++ components/Nova.vue
@@ -1,5 +1,16 @@
 <template>
 <div>
-hello world
+<span id="valor">hello world</span>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<script>
+function verValor() {
+	var url="https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=FD3DD3DE-FD89-4F89-B5CB-49F798890CEC";
+	$.getJSON( url, function( json ) {
+		$('#valor').html('Bitcoin = $' + json.rate);
+	});
+}
+</script>
+<br>
+<button onclick="verValor()">Ver valor do Bitcoin</button>
 </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,4 +6,6 @@
 export default {
   name: 'IndexPage'
 }
+
+
 </script>
